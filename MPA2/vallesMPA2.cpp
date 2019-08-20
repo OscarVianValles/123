6f7f6916fc67a488353811b27bb066f343be44db
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-08-20 12:13:58 +0800

    added tokenizing the paren content

--- MPA2/vallesMPA2.cpp
+++ MPA2/vallesMPA2.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -7,22 +8,24 @@
 
 class Loop {
 public:
-  std::string getIdentifier();
+  std::string getIterator();
   std::string getCondition();
   std::string getOperator();
   std::string getProcedures();
 
-  void tokenize(std::string file);
+  void tokenize(std::string &file);
+  void tokenizeParenContent(std::string &parenContent);
+  void tokenizeBracketContent(std::string &bracketContent);
   void printMembers();
 
 private:
-  std::string identifier;
+  std::string iterator;
   std::string condition;
   std::string loopOperator; // Operator is a reserved word
   std::string procedures;
 };
 
-void Loop::tokenize(std::string file) {
+void Loop::tokenize(std::string &file) {
   // Gets the next character after the parenthesis
   unsigned openParen = file.find("(") + 1;
 
@@ -38,8 +41,52 @@ void Loop::tokenize(std::string file) {
   unsigned bracketLen = file.find("}") - openBracket;
 
   // Gets the content of the parenthesis
+
   std::string parenContent = file.substr(openParen, parenLen);
   std::string bracketContent = file.substr(openBracket, bracketLen);
+
+  tokenizeParenContent(parenContent);
+}
+
+void Loop::tokenizeParenContent(std::string &parenContent) {
+
+  // Creating buffer variables
+  std::istringstream tempStream(parenContent);
+  std::string token;
+  int counter = 0;
+
+  // Tokenizing the string
+  while (std::getline(tempStream, token, ';')) {
+    switch (counter) {
+    case 0:
+      iterator = token;
+      break;
+    case 1:
+      condition = token;
+      break;
+    case 2:
+      loopOperator = token;
+      break;
+    default:
+      break;
+    }
+
+    counter++;
+  }
+}
+
+void Loop::tokenizeBracketContent(std::string &bracketContent) {
+
+  // Creating buffer variables
+  std::istringstream tempStream(bracketContent);
+  std::string token;
+}
+
+void Loop::printMembers() {
+  std::cout << "Iterator: " << iterator << std::endl
+            << "Condition: " << condition << std::endl
+            << "Operator: " << (loopOperator.empty() ? "Empty" : loopOperator)
+            << std::endl;
 }
 
 std::string readFile(std::ifstream &input);
@@ -56,6 +103,8 @@ int main() {
   Loop *first = new Loop();
 
   first->tokenize(file);
+  first->printMembers();
+
   return 0;
 }
 
@@ -77,6 +126,10 @@ std::string readFile(std::ifstream &input) {
       fileContent.append(lines);
     }
   }
+
+  // Removing Spaces
+  fileContent.erase(std::remove(fileContent.begin(), fileContent.end(), ' '),
+                    fileContent.end());
 
   return fileContent;
 }
