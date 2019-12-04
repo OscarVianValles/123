a218312ae242398f90af60897dbb018b8b2fc9ea
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-12-04 09:47:39 +0800

    file descriptor added

---
+++ MPA5/fileDescriptor.cpp
@@ -0,0 +1,31 @@
+#include "fileDescriptor.hpp"
+
+Descriptor::Descriptor(bool isFile, std::string name, std::string fileContent) {
+  __isFile = isFile;
+  __created = time(NULL);
+  __modified = __created;
+  __name = name;
+  __fileContent = fileContent;
+}
+
+bool Descriptor::isFile() { return __isFile; }
+
+time_t Descriptor::created() { return __created; }
+
+time_t Descriptor::modfied() { return __modified; }
+
+std::string Descriptor::name() { return __name; }
+
+std::string Descriptor::fileContent() { return __fileContent; }
+
+void Descriptor::changeName(std::string x) {
+  __name = x;
+  __modified = time(NULL);
+}
+
+void Descriptor::appendFileContent(std::string x) {
+  __fileContent += x;
+  __modified = time(NULL);
+}
+
+void Descriptor::modify() { __modified = time(NULL); }
---
+++ MPA5/fileDescriptor.hpp
@@ -0,0 +1,31 @@
+#ifndef MPA5_FILE_DESCRIPTOR
+#define MPA5_FILE_DESCRIPTOR
+
+#include <ctime>
+#include <string>
+
+class Descriptor {
+private:
+  bool __isFile;
+  time_t __modified;
+  time_t __created;
+  std::string __name;
+  std::string __fileContent;
+
+public:
+  // Constructor
+  Descriptor(bool, std::string, std::string);
+
+  // Builders
+  bool isFile();
+  time_t created();
+  time_t modfied();
+  std::string name();
+  std::string fileContent();
+
+  void changeName(std::string);
+  void appendFileContent(std::string);
+  void modify();
+};
+
+#endif
