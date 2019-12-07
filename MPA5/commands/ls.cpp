b746e43b343d3a07ae3c840f4c8c390bb423339e
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-12-07 16:21:02 +0800

    working regex search ls

--- MPA5/commands/ls.cpp
+++ MPA5/commands/ls.cpp
@@ -7,10 +7,68 @@ ls::~ls() {}
 bool ls::execute(FileTree &t) {
   if (params.empty()) {
     Node *curr = t.current();
-    for (auto &child : curr->children) {
+    __printDirectory(curr);
+    return true;
+  } else {
+    std::list<std::string> tokens = tokenize(params.front(), '/');
+    if (tokens.back() == "") {
+      tokens.pop_back();
+    }
+
+    // Try searching for directory
+    Node *currNode = t.search(tokens, false);
+    if (currNode) {
+      __printDirectory(currNode);
+    } else {
+      // If currNode was not found, then try if the last string is a valid
+      // regex, if it is not valid, then it returns an error
+      std::regex pattern(".*");
+      try {
+        std::string possibleRegex = tokens.back();
+        if (possibleRegex[0] == '*' && possibleRegex[1] == '.') {
+          possibleRegex = "." + possibleRegex + "$";
+        }
+        std::cout << possibleRegex << std::endl;
+
+      } catch (std::regex_error &e) {
+        std::cout << "ls: cannot access '" + params.front() +
+                         "': No such file or directory"
+                  << std::endl;
+        return false;
+      }
+
+      tokens.pop_back();
+      Node *currNode = t.current();
+      if (tokens.size() >= 1) {
+        currNode = t.search(tokens, false);
+
+        if (!currNode) {
+          std::cout << "ls: cannot access '" + params.front() +
+                           "': No such file or directory"
+                    << std::endl;
+          return false;
+        }
+      }
+
+      __printDirectory(currNode, pattern);
+    }
+  }
+
+  return true;
+}
+
+void ls::__printDirectory(Node *curr) {
+  for (auto &child : curr->children) {
+    std::cout << child->content.name() << (child->content.isFolder() ? "/" : "")
+              << std::endl;
+  }
+}
+
+void ls::__printDirectory(Node *curr, std::regex pattern) {
+  for (auto &child : curr->children) {
+    if (std::regex_match(child->content.name(), pattern)) {
       std::cout << child->content.name()
                 << (child->content.isFolder() ? "/" : "") << std::endl;
     }
-    return true;
   }
 }
--- MPA5/commands/ls.hpp
+++ MPA5/commands/ls.hpp
@@ -4,9 +4,14 @@
 #include "command.hpp"
 #include <iostream>
 #include <list>
+#include <regex>
 #include <string>
 
 class ls : public Command {
+private:
+  void __printDirectory(Node *, std::regex);
+  void __printDirectory(Node *);
+
 public:
   ls(std::list<std::string>);
   ~ls();
